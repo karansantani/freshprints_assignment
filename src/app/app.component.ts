0f8d2d7cd45bef1fe7b73609839098e0b843bcb6
Karan Santani <karansantani@gmail.com>
2020-06-14 21:29:38 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate, group } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'FPSignIn';
+export class AppComponent implements OnInit{
+  title = 'Fresh Prints';
+  toggle: boolean;
+  domToggle: boolean;
+
+  ngOnInit() {
+    this.toggle = true;
+    this.domToggle = true;
+  }
+
+  toggleForms = () => {
+    this.toggle = !this.toggle;
+    setTimeout(() => {
+      this.domToggle = this.toggle;
+    }, 200);
+  }
 }
